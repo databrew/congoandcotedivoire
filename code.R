52796c4c071e0b23d5a73faf91ac22208b1322b7
Joe Brew <joebrew@gmail.com>
2023-08-15 18:24:59 +0200

    updated

--- code.R
+++ code.R
@@ -30,7 +30,6 @@ for(j in 1:ncol(congo)){
 var_names <- names(congo)
 date_vars <- c()
 for(j in 1:ncol(congo)){
-  message(j)
   vals <- unlist(congo[,j])
   is_character <- class(vals) == 'character'
   if(is_character){
@@ -47,7 +46,6 @@ date_vars <- c(date_vars, 'last_menstrual_period', names(congo)[grepl('date', to
 date_vars <- sort(unique(date_vars))
 # Change date variables to date format
 for(j in 1:length(date_vars)){
-  message(j)
   this_var <- date_vars[j]
   vals <- unlist(congo[,this_var])
   vals <- as.character(vals)
@@ -165,17 +163,98 @@ ggplot(data = pd,
 
 # Mean and range per month (mean of daily tests)
 pd <- cote %>%
+  group_by(date) %>%
+  summarise(n_tests = n()) %>%
+  ungroup %>%
   group_by(month = lubridate::floor_date(date, 'month')) %>%
-  summarise(avg )
+  summarise(avg_daily_tests = mean(n_tests),
+            min_daily_tests = min(n_tests),
+            max_daily_tests = max(n_tests),
+            p25_daily_tests = quantile(n_tests, 0.25),
+            p75_daily_tests = quantile(n_tests, 0.75))
+ggplot(data = pd) +
+  geom_ribbon(aes(x = month,
+                  ymin = p25_daily_tests,
+                  ymax = p75_daily_tests),
+              fill = 'orange') +
+  geom_line(aes(x = month,
+           y = avg_daily_tests))
 
 # Type of test (GE = gout espese, TDR = RDT) per month
+pd <- cote %>%
+  group_by(month = lubridate::floor_date(date, 'month'),
+           test) %>%
+  tally
+ggplot(data = pd,
+       aes(x = month,
+           y = n,
+           fill = test,
+           group = test)) +
+  geom_bar(stat = 'identity',
+           position = position_stack())
+  
+
+
 # Test positivity rate per month
 # Simple
+pd <- cote %>%
+  group_by(month = lubridate::floor_date(date, 'month')) %>%
+  summarise(n_tests = n(),
+            n_positve = sum(result)) %>%
+  ungroup %>%
+  mutate(p_positive = n_positve / n_tests * 100)
+ggplot(data = pd,
+       aes(x = month,
+           y = p_positive)) +
+  geom_line()
+  
 # Mean and range
+mean(pd$p_positive, na.rm = TRUE)
+range(pd$p_positive, na.rm = TRUE)
+
 # positivity rate by sex (monthly)
+pd <- cote %>%
+  mutate(sex = toupper(sex)) %>%
+  filter(!is.na(sex)) %>%
+  group_by(month = lubridate::floor_date(date, 'month'),
+           sex) %>%
+  summarise(n_tests = n(),
+            n_positve = sum(result)) %>%
+  ungroup %>%
+  mutate(p_positive = n_positve / n_tests * 100)
+ggplot(data = pd,
+       aes(x = month,
+           y = p_positive,
+           color = sex)) +
+  geom_line() +
+  facet_wrap(~sex) +
+  theme(legend.position = 'none')
 # posititivty rate by age tranch (<5, 5-15, >15) (monthly)
+pd <- cote %>%
+  mutate(age = `age (years)`) %>%
+  filter(!is.na(age)) %>%
+  mutate(age = cut(age, c(0, 6, seq(6.01, 66, 12), 100))) %>%
+  filter(!is.na(age)) %>%
+  group_by(month = lubridate::floor_date(date, 'month'),
+           age) %>%
+  summarise(n_tests = n(),
+            n_positve = sum(result)) %>%
+  ungroup %>%
+  mutate(p_positive = n_positve / n_tests * 100)
+ggplot(data = pd,
+       aes(x = month,
+           y = p_positive,
+           color = age)) +
+  geom_point(aes(size = n_tests)) +
+  geom_line() +
+  facet_wrap(~age) +
+  theme(legend.position = 'none')
 # Mean age of those with positive tests
+mean(cote$`age (years)`[cote$result == 1], na.rm = TRUE)
+
+# (got to here)
 # Rains per month
+
 # Simple
 # Mean and range
 # Correlation rain-test positivity rate (monthly)
